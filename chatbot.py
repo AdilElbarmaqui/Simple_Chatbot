2427240ebe7ae13beca0f768f14180d62c0390df
AdilElbarmaqui <adil.elbarmaqui@gmail.com>
2023-09-03 01:04:57 +0100

    Final version of chatbot with memory maintain

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,33 @@
 import openai
 
+# Initialize your conversation memory as an empty dictionary
+conversation_memory={}
+
 # Setting your OpenAI API key 
-openai.api_key = 'your_API_Key'
+openai.api_key = 'Your_Api_Key'
 
 
 def generate_response(input_text):
+    
+    conversation_memory["user_input"] = input_text
+    
+    # Use the conversation history in the prompt
+    prompt = "\n".join(list(conversation_memory.values()))
+    
     response = openai.Completion.create(
         model = "text-davinci-003",  # or another GPT-3 engine
-        prompt=input_text,
+        prompt=prompt,
         max_tokens=1000, # Adjust as needed
         n=1, 
         stop = None,
-        temperature = 0.5,
-        )
-    return response.choices[0].text.strip()
+        temperature = 0.5,)
+    
+    reply = response.choices[0].text.strip()
+    
+    # Add the chatbot's reply to the conversation memory
+    conversation_memory["chatbot_reply"] = reply
+    
+    return reply
 
 
 
